55d29ca520dd196b23822305816f9b51e83b20e5
user896724 <>
2020-05-06 12:06:34 +0100

    move common head markup from boostrap html to App.svelte; add ${head} for live reloads; add global css inc. google font; add 404 route

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -3,6 +3,7 @@ import {setContext} from "svelte";
 import payload from "svelte-view-engine/payload";
 import {Router, Route} from "svelte-routing";
 import axios from "axios";
+import _404 from "./pages/404.svelte";
 import Index from "./pages/Index.svelte";
 
 let {
@@ -16,6 +17,24 @@ let backend = axios.create({
 setContext("backend", backend);
 </script>
 
+<svelte:head>
+	<title>Timesheets</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;700&display=swap" rel="stylesheet">
+	<style>
+		body {
+			font-family: "Nunito Sans", sans-serif;
+			letter-spacing: .5px;
+			margin: 0;
+		}
+		
+		div, form {
+			box-sizing: border-box;
+		}
+	</style>
+</svelte:head>
+
 <Router>
 	<Route path="/" component={Index}/>
+	<Route component={_404}/>
 </Router>
---
+++ src/app/pages/404.svelte
@@ -0,0 +1,11 @@
+<script>
+import Main from "./Main.svelte";	
+</script>
+
+<style>
+
+</style>
+
+<Main>
+	404
+</Main>
