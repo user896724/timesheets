b017b3e1139516315bc9441d41d94d5b6b3a7573
user896724 <>
2020-05-07 12:33:23 +0100

    add some responses to responses util; use http-status-codes constants

--- src/utils/responses.js
+++ src/utils/responses.js
@@ -1,18 +1,35 @@
-module.exports = {
-	notFound(res) {
-		res.status(404);
-		res.json(null);
-	},
-	
-	serverError(res, error) {
-		console.error(error);
-		
-		res.status(500);
+let HttpStatus = require("http-status-codes");
+
+function error(code) {
+	return function(res, error=null) {
+		res.status(code);
 		
 		try {
 			res.json(error);
 		} catch (e) {
-			res.json("Unknown error");
+			res.json(null);
 		}
+	}
+}
+
+let serverError = error(HttpStatus.INTERNAL_SERVER_ERROR);
+
+function ok(code) {
+	return function(res, json=null) {
+		res.status(HttpStatus.CREATED);
+		res.json(json);
+	}
+}
+
+module.exports = {
+	notFound: error(HttpStatus.NOT_FOUND),
+	conflict: error(HttpStatus.CONFLICT),
+	badRequest: error(HttpStatus.BAD_REQUEST),
+	
+	created: ok(HttpStatus.CREATED),
+	
+	serverError(res, error) {
+		console.error(error);
+		serverError(res, error);
 	},
 };
