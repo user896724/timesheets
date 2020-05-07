47948446bee1ea66fde1f93129134475a8c3659a
user896724 <>
2020-05-07 14:21:42 +0100

    remove test route

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app, core, db) {
-	app.get("/", async function(req, res) {
-		await app.mail("to", "subject", "Welcome", {
-			name: "test",
-		});
-		
-		res.end("");
-	});
-}
