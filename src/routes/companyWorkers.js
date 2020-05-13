1cc97c27c92f5b59bf61c74bf569cd6febc41099
user896724 <>
2020-05-13 14:30:21 +0100

    reinstate company workers post route (overwritten by mistake)

--- src/routes/companyWorkers.js
+++ src/routes/companyWorkers.js
@@ -1,9 +1,11 @@
-let {unauthorized, notFound, ok} = require("../utils/responses");
+let {unauthorized, notFound, created, ok} = require("../utils/responses");
 let requireAuth = require("../middleware/requireAuth");
 
 module.exports = function(app, core, db) {
 	let {
 		Company,
+		Invite,
+		relationships,
 	} = core;
 	
 	app.get("/company/:id/workers", requireAuth, async function(req, res) {
@@ -23,6 +25,30 @@ module.exports = function(app, core, db) {
 		let workers = await company.workers;
 		
 		ok(res, workers);
+	});
+	
+	app.post("/company/:id/workers", requireAuth, async function(req, res) {
+		let companyId = Number(req.params.id);
+		let {userId} = req.body;
+		let {user} = req;
+		
+		if (userId !== user.id) {
+			return unauthorized(res);
+		}
+		
+		let invite = await Invite.find({
+			companyId,
+			email: user.email,
+		});
+		
+		if (!invite) {
+			return unauthorized(res);
+		}
+		
+		await relationships.joinCompany(companyId, userId);
+		await invite.delete();
+		
+		created(res);
 	});
 	
 	app.delete("/company/:companyId/worker/:userId", requireAuth, async function(req, res) {
